ed8e5d57cf17b7b287642bf80ae2edc650440674
Vedant Rokde <vedant24rokde@gmail.com>
2022-07-01 10:44:13 +0530

    Add CORS

--- stock-market/src/main/java/com/code/stockmarket/controller/StockController.java
+++ stock-market/src/main/java/com/code/stockmarket/controller/StockController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.code.stockmarket.model.Stock;
 import com.code.stockmarket.service.serviceImpl.StockService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1.0/market/stock/")
 public class StockController {
     @Autowired
