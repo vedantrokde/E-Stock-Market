0bd04ed27113810174ebc5046c2b93626bac37ad
Vedant Rokde <vedant24rokde@gmail.com>
2022-07-03 23:51:37 +0530

    Fix minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-//import {MaterialModule} from '@angular/material';
-//import {MatDatepickerModule} from '@angular/material/datepicker';
-import {HttpClient,HttpClientModule,HttpClientXsrfModule,HttpRequest} from '@angular/common/http'
-import { AgGridModule } from 'ag-grid-angular';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpClientModule, HttpClientXsrfModule } from "@angular/common/http";
+import { AgGridModule } from "ag-grid-angular";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AgGridModule,
-    NgbModule,
-    HttpClientModule,
-    HttpClientXsrfModule
-  //  MatDatepickerModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AgGridModule,
+		NgbModule,
+		HttpClientModule,
+		HttpClientXsrfModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
